9e08e0430477d256d0963ff542b235a393a5fa1b
12429611 <54491432+12429611@users.noreply.github.com>
2019-10-14 01:58:33 +1100

    Pacman
    almost finish. coding will be add some recommed.

---
+++ Pacman/Assets/Scripts/HardButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HardButton : MonoBehaviour
+{
+
+    public Button mButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Gets Button
+        Button btnMount = mButton.GetComponent<Button>();
+        //add a listener to Button, executing TaskOnClick() when click Button
+        btnMount.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Loading Scene1
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
